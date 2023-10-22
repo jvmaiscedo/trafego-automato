680eaa1d3b7569dfcfb69ed3eaa90c956ba8b212
João Victor Macedo <gmvictorr@outlook.com>
2023-10-21 21:52:33 -0300

    alteracao na classe filosofo

--- controller/MainController.java
+++ controller/MainController.java
@@ -12,17 +12,31 @@ package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.concurrent.Semaphore;
 
 import javafx.fxml.Initializable;
+import model.Filosofo;
 
 
 public class MainController implements Initializable {
 
+  public static final int N = 5; // define o no. de filosofos
+  public static Filosofo[] filosofos = new Filosofo[N];
+  public static int[] states = new int[N];
+  public static Semaphore mutex = new Semaphore(1);
+  public static Semaphore[] forks = new Semaphore[N];
+
   @Override
   public void initialize(URL url, ResourceBundle rb) {
-
+    for(int i=0; i<N ;i++){
+      forks [i] = new Semaphore(1);
+      filosofos[i] = new Filosofo(i, this );
+      states[i] = 0;
+    }
+    for (int j=0; j<N; j++){
+      filosofos[j].setNeibhours();
+      filosofos[j].start();
+    }
   }
-
-
-
+  
 }
--- model/Filosofo.java
+++ model/Filosofo.java
@@ -1,13 +1,81 @@
 package model;
 
+import controller.MainController;
+import sun.applet.Main;
+
+import java.util.concurrent.Semaphore;
+
 public class Filosofo extends Thread{
 
+  private MainController control;
+  private int id;
+  private Filosofo left;
+  private Filosofo right;
+  public Filosofo(int index, MainController control){
+    this.id = index;
+    this.control = control;
+    this.left = MainController.filosofos[(this.id - 1 +MainController.N) % MainController.N];
+    this.right = MainController.filosofos[(this.id + 1 +MainController.N) % MainController.N];
 
+  }
+  @Override
+  public void run() {
+    while(true) {
+      try {
+        think();
+        take_forks();
+        eat();
+        put_forks();
+        sleep(20);
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+    }
+  }
 
+  public void setNeibhours(){
+    this.left = MainController.filosofos[(this.id - 1 +MainController.N) % MainController.N];
+    this.right = MainController.filosofos[(this.id + 1 +MainController.N) % MainController.N];
+  }
 
+  private void think(){
+    try {
+      System.out.println("O filosofo "+this.id+" esta ensando");
+      sleep(1500);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+  }
+  private void take_forks(){
+    try {
+      MainController.mutex.acquire();
+      MainController.states[id] = 1;
+      Filosofo.test(this);
+      MainController.mutex.release();
+      MainController.forks[id].acquire();
+      //MainController.hideForks -> fazer posteriormente.
+    } catch (InterruptedException e) {}
 
-  private void think(){}
-  private void take_forks(){}
-  private void eat(){}
-  private void put_forks(){}
+  }
+  private void eat(){
+    System.out.println("O filosofo "+this.id+" esta comendo");
+  }
+  private void put_forks(){
+    try {
+      MainController.mutex.acquire();
+      MainController.states[id] = 0;
+      Filosofo.test(left);
+      Filosofo.test(right);
+      MainController.mutex.release();
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+  }
+  public static void test(Filosofo filosofo){
+    if(MainController.states[filosofo.id]==1 && MainController.states[filosofo.left.id]!=2
+        && MainController.states[filosofo.right.id]!=2){
+      MainController.states[filosofo.id] = 2;
+      MainController.forks[filosofo.id].release();
+    }
+  }
 }
