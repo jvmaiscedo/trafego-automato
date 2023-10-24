c32461785f4c321bb040e3c1796dcdb0c0497c4b
João Victor Macedo <gmvictorr@outlook.com>
2023-10-24 19:50:37 -0300

    adição de elementos gráficos

--- controller/MainController.java
+++ controller/MainController.java
@@ -14,16 +14,49 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.concurrent.Semaphore;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
 import model.Filosofo;
 
 
 public class MainController implements Initializable {
 
+  @FXML
+  public ImageView bateristaEscutando1;
+  @FXML
+  public ImageView bateristaEscutando2;
+  @FXML
+  public ImageView bateristaEscutando3;
+  @FXML
+  public ImageView bateristaEscutando4;
+  @FXML
+  public ImageView bateristaEscutando5;
+  @FXML
+  public ImageView bateristaTocando1;
+  @FXML
+  public ImageView bateristaTocando2;
+  @FXML
+  public ImageView bateristaTocando3;
+  @FXML
+  public ImageView bateristaTocando4;
+  @FXML
+  public ImageView bateristaTocando5;
+  @FXML
+  public ImageView baqueta1;
+  @FXML
+  public ImageView baqueta2;
+  @FXML
+  public ImageView baqueta3;
+  @FXML
+  public ImageView baqueta4;
+  @FXML
+  public ImageView baqueta5;
+
   public static final int N = 5; // define o no. de filosofos
   public static Filosofo[] filosofos = new Filosofo[N];
   public static int[] states = new int[N];
-  public static Semaphore mutex = new Semaphore(1);
+  public volatile static Semaphore mutex = new Semaphore(1);
   public static Semaphore[] forks = new Semaphore[N];
 
   @Override
@@ -34,9 +67,132 @@ public class MainController implements Initializable {
       states[i] = 0;
     }
     for (int j=0; j<N; j++){
-      filosofos[j].setNeibhours();
-      filosofos[j].start();
+      filosofos[j].setNeighbours();
     }
+    filosofos[0].start();
+    filosofos[1].start();
+    filosofos[2].start();
+    filosofos[3].start();
+    filosofos[4].start();
+  }
+
+  public void changeImageToPlaying(int id){
+    if(id==0){
+      bateristaEscutando1.setVisible(false);
+      bateristaTocando1.setVisible(true);
+    }
+    else if(id==1){
+      bateristaEscutando2.setVisible(false);
+      bateristaTocando2.setVisible(true);
+    }
+    else if(id==2){
+      bateristaEscutando3.setVisible(false);
+      bateristaTocando3.setVisible(true);
+    }
+    else if(id==3){
+      bateristaEscutando4.setVisible(false);
+      bateristaTocando4.setVisible(true);
+    }
+    else if(id==4){
+      bateristaEscutando5.setVisible(false);
+      bateristaTocando5.setVisible(true);
+    }
+
+  }
+
+  public void changeImageToListening(int id){
+    if(id==0){
+      bateristaTocando1.setVisible(false);
+      bateristaEscutando1.setVisible(true);
+    }
+    else if(id==1){
+      bateristaTocando2.setVisible(false);
+      bateristaEscutando2.setVisible(true);
+    }
+    else if(id==2){
+      bateristaTocando3.setVisible(false);
+      bateristaEscutando3.setVisible(true);
+    }
+    else if(id==3){
+      bateristaTocando4.setVisible(false);
+      bateristaEscutando4.setVisible(true);
+    }
+    else if(id==4){
+      bateristaTocando5.setVisible(false);
+      bateristaEscutando5.setVisible(true);
+    }
+
+  }
+
+  public void changeImageToWaiting(int id){
+    if(id==0){
+      bateristaTocando1.setVisible(false);
+      bateristaEscutando1.setVisible(false);
+    }
+    else if(id==1){
+      bateristaTocando2.setVisible(false);
+      bateristaEscutando2.setVisible(false);
+    }
+    else if(id==2){
+      bateristaTocando3.setVisible(false);
+      bateristaEscutando3.setVisible(false);
+    }
+    else if(id==3){
+      bateristaTocando4.setVisible(false);
+      bateristaEscutando4.setVisible(false);
+    }
+    else if(id==4){
+      bateristaTocando5.setVisible(false);
+      bateristaEscutando5.setVisible(false);
+    }
+
+  }
+
+  public void hideSticks(int id){
+    if(id==0){
+      baqueta1.setVisible(false);
+      baqueta2.setVisible(false);
+    }
+    else if (id==1) {
+      baqueta2.setVisible(false);
+      baqueta3.setVisible(false);
+    }
+    else if (id==2) {
+      baqueta3.setVisible(false);
+      baqueta4.setVisible(false);
+    }
+    else if (id==3) {
+      baqueta4.setVisible(false);
+      baqueta5.setVisible(false);
+    }
+    else if (id==4) {
+      baqueta5.setVisible(false);
+      baqueta1.setVisible(false);
+    }
+
+  }
+  public void showSticks(int id){
+    if(id==0){
+      baqueta1.setVisible(true);
+      baqueta2.setVisible(true);
+    }
+    else if (id==1) {
+      baqueta2.setVisible(true);
+      baqueta3.setVisible(true);
+    }
+    else if (id==2) {
+      baqueta3.setVisible(true);
+      baqueta4.setVisible(true);
+    }
+    else if (id==3) {
+      baqueta4.setVisible(true);
+      baqueta5.setVisible(true);
+    }
+    else if (id==4) {
+      baqueta5.setVisible(true);
+      baqueta1.setVisible(true);
+    }
+
   }
   
 }
--- model/Filosofo.java
+++ model/Filosofo.java
@@ -1,47 +1,45 @@
 package model;
 
 import controller.MainController;
-import sun.applet.Main;
-
-import java.util.concurrent.Semaphore;
+import javafx.application.Platform;
 
 public class Filosofo extends Thread{
 
-  private MainController control;
-  private int id;
+  private final MainController control;
+  private final int id;
   private Filosofo left;
   private Filosofo right;
   public Filosofo(int index, MainController control){
     this.id = index;
     this.control = control;
-    this.left = MainController.filosofos[(this.id - 1 +MainController.N) % MainController.N];
-    this.right = MainController.filosofos[(this.id + 1 +MainController.N) % MainController.N];
-
   }
+
   @Override
   public void run() {
     while(true) {
-      try {
-        think();
-        take_forks();
-        eat();
-        put_forks();
-        sleep(20);
-      } catch (InterruptedException e) {
-        throw new RuntimeException(e);
-      }
+      think();
+      take_forks();
+      eat();
+      put_forks();
+
     }
   }
 
-  public void setNeibhours(){
-    this.left = MainController.filosofos[(this.id - 1 +MainController.N) % MainController.N];
-    this.right = MainController.filosofos[(this.id + 1 +MainController.N) % MainController.N];
+  public void setNeighbours(){
+    this.left = MainController.filosofos[(this.id - 1 + MainController.N) % MainController.N];
+    this.right = MainController.filosofos[(this.id + 1 + MainController.N) % MainController.N];
+    System.out.println("Esquerda: "+left.id + " . Direita" + right.id);
   }
 
   private void think(){
     try {
-      System.out.println("O filosofo "+this.id+" esta ensando");
+      Platform.runLater(() -> control.changeImageToListening(this.id));
+      System.out.println("O filosofo " + this.id + " está pensando");
       sleep(1500);
+      Platform.runLater(() -> control.changeImageToWaiting(this.id));
+      sleep(1500);
+      System.out.println("Filosofo " + this.id + " passou por pensar");
+
     } catch (InterruptedException e) {
       throw new RuntimeException(e);
     }
@@ -49,27 +47,36 @@ public class Filosofo extends Thread{
   private void take_forks(){
     try {
       MainController.mutex.acquire();
-      MainController.states[id] = 1;
+      MainController.states[this.id] = 1;
       Filosofo.test(this);
+      System.out.println("Filosofo " + this.id + " tentando pegar garfos");
       MainController.mutex.release();
-      MainController.forks[id].acquire();
-      //MainController.hideForks -> fazer posteriormente.
+      MainController.forks[this.id].acquire();
+      Platform.runLater(()->control.hideSticks(this.id));
+      System.out.println("Filosofo " + this.id + " pegou os garfos");
     } catch (InterruptedException e) {}
 
   }
-  private void eat(){
-    System.out.println("O filosofo "+this.id+" esta comendo");
+  private void eat() {
+    try  {
+      System.out.println("O filosofo " + this.id + " está comendo");
+      Platform.runLater(() -> control.changeImageToPlaying(this.id));
+      sleep(2000);
+    }catch (InterruptedException e) {}
   }
   private void put_forks(){
     try {
+      Platform.runLater(()->control.showSticks(this.id));
+      Platform.runLater(() -> control.changeImageToListening(this.id));
       MainController.mutex.acquire();
-      MainController.states[id] = 0;
+      MainController.states[this.id] = 0;
       Filosofo.test(left);
       Filosofo.test(right);
       MainController.mutex.release();
     } catch (InterruptedException e) {
       throw new RuntimeException(e);
     }
+    System.out.println("Filosofo " + this.id + " colocou os garfos");
   }
   public static void test(Filosofo filosofo){
     if(MainController.states[filosofo.id]==1 && MainController.states[filosofo.left.id]!=2
@@ -77,5 +84,6 @@ public class Filosofo extends Thread{
       MainController.states[filosofo.id] = 2;
       MainController.forks[filosofo.id].release();
     }
+
   }
 }
